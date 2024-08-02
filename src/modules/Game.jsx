8ff3f06facdf02d1f875460093816706cdf84f58
Andres Muñoz <andrefe91@gmail.com>
2024-08-01 19:08:00 -0500

    Start of game module

--- src/modules/Game.jsx
+++ src/modules/Game.jsx
@@ -1,16 +1,17 @@
 import '../styles/Game.css'
 import { useState } from 'react'
 import reorder from '../scripts/reorder';
+import { getCache } from '../scripts/loadAndSave';
 
 export default function Game({list}) {
     const [gameList, setGameList] = useState(list);
+    const cache = getCache();
 
     function handleReorder() {
         let reorderedList = reorder([...gameList]);
         setGameList([...reorderedList]);
     }
 
-    console.log(gameList);
 
     return (
         <>
